af323a08c12b7bfd7a5aed907bf9d3457318b27f
Ioodu <chinadbo@foxmail.com>
2019-03-15 10:35:36 +0800

    Initial

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  title: 'Ioodu Land',
+  description: "Ioodu's home page →",
+  head:[
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+  dest: 'dist',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Blog', link: 'https://chinadbo.github.io/blog/' }
+    ],
+    sidebar: 'auto',
+    search: false,
+    lastUpdated: 'Last Updated',
+    repo: 'chinadbo',
+    repoLabel: 'Github Pages',
+    docsRepo: 'chinadbo/chinadbo.github.io',
+    docsDir: 'docs',
+    docsBranch: 'master',
+    editLinks: true,
+    editLinkText: '帮助改善页面！',
+    copyRight: 'Ioodu@2014-present'
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,27 @@
+---
+home: true
+meta:
+  - name: description
+    content: Ioodu's Home Page
+  - name: keywords
+    content: Ioodu
+heroImage: /i.jpg
+title: Ioodu
+actionText: Blog Start →
+actionLink: https://chinadbo.github.io/blog/
+features:
+- title: Everything
+  details: Everything other → 
+footer: MIT Licensed | Copyright © 2018-present Ioodu
+---
+
+::: tip
+Please contact me:
+
+📧 Email: chinadbo#gmail.com
+
+🏠 Address: ShenZhen, China
+
+🔗 Website: www.ioodu.com
+
+:::
